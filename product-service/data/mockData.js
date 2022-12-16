dfc932b2b7aacb56f9b590d8440249606260fc06
Alexander Krasovsky <Alexander_Krasovsky@epam.com>
2022-12-16 03:33:38 +0300

    feat: create build and handlers

---
+++ product-service/data/mockData.js
@@ -0,0 +1,18 @@
+const mockData = [
+  {
+    title: "Iphone 14",
+    description: "Short Product Description1",
+    price: 1000,
+    image: "https://content2.onliner.by/catalog/device/header/d68e83aa99a0b98f2990da108fdd3eec.jpeg",
+    id: "1"
+  },
+  {
+    title: "Iphone 14 pro",
+    description: "Short Product Description2",
+    price: 1200,
+    image: "https://content2.onliner.by/catalog/device/main/ad1fd08115cc6e1b4c289d580d79b406.jpeg",
+    id: "2"
+  }
+];
+
+export default mockData;
--- product-service/handler.js
+++ product-service/handler.js
@@ -1,16 +1,4 @@
-'use strict';
+import { getProductsList } from './handlers/getProductsList';
+import { getProductsById } from './handlers/getProductsById';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        product: 'Iphone 14',
-        price: '1000$',
-      },
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+export { getProductsList, getProductsById };
